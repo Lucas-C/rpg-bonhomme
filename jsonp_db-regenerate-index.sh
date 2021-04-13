59cf9644895eed6e2af0017af96a63936c22a116
Lucas Cimon <925560+Lucas-C@users.noreply.github.com>
2021-04-13 22:18:52 +0200

    Minor fixup

--- jsonp_db-regenerate-index.sh
+++ jsonp_db-regenerate-index.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -o pipefail -o errexit -o nounset -o xtrace
 
+date
+
 cd $(dirname $(readlink -f "${BASH_SOURCE[0]}"))
 
 PATH=$PATH:/usr/local/bin/
-pew in rpg-bonhomme make
+pew in rpg-bonhomme make -B
